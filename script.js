8e1361ab7531724e5e869049d48861e000d4f72b
Oleksandr <oleksandr@Oleksandrs-MacBook-Pro.local>
2024-06-30 10:47:26 +0200

    help

--- script.js
+++ script.js
@@ -2,23 +2,30 @@ const normalAttackElement = document.querySelector('.action-attack');
 const ultAttackElement = document.querySelector('.action-ult-attack');
 const miscElement = document.querySelector('.action-special');
 
+const clicksElement = document.querySelector('.total-clicks');
 const enemyHpElement = document.querySelector('.enemy-hp');
 let constantEnemyHp = 100;
 let enemyHp = 100;
+
+let totalClicks = 0;
+let ultClicks = 0;
+
+let ultReadiness = false;
+
+clicksElement.innerHTML = `Total clicks: ${totalClicks}`;
 
 enemyHpElement.innerHTML = `Hp: ${enemyHp}`;
 
 let character = {
     level: 1,
     exp: 0,
-}
+};
 
 const battleRating = character.level * 100;
 
-console.log(battleRating);
-
-function updateHpElement () {
+function updateElements () {
     enemyHpElement.innerHTML = `Hp: ${enemyHp}`;
+    clicksElement.innerHTML = `Total clicks: ${totalClicks}`;
 };
 
 function calculateDamage (type) {
@@ -34,21 +41,41 @@ function calculateDamage (type) {
         default:
             break;
     }
-}
+};
+
+function handleUltClicks () {
+    if (ultClicks >= 20) {
+        ultReadiness = true;
+        ultClicks = 0;
+
+        alert('Your ULT is ready!')
+    } else {
+        return;
+    };
+};
 
 function normalAttack (damage) {
     enemyHp -= damage;
     handleEnemyDeath()
-    updateHpElement();
+    updateElements();
     console.log(`Damaged: ${damage}`);
-}
+};
 
 function ultAttack (damage) {
-    enemyHp -= damage;
-    handleEnemyDeath()
-    updateHpElement()
-    console.log(`Damaged: ${damage}`);
-}
+    if (ultReadiness === true) {
+        enemyHp -= damage;
+
+        handleEnemyDeath();
+        updateElements();
+
+        console.log(`Damaged: ${damage}`);
+
+        ultReadiness === false;
+    } else {
+        alert('ULT is not ready!');
+        return;
+    };
+};
 
 function specialAbil () {
     alert('Under development')
@@ -68,27 +95,34 @@ function handleExp(amount) {
     } else {
         return
     }
-}
+};
 
 function handleEnemyDeath () {
     if (enemyHp <= 0) {
         enemyHp = constantEnemyHp * 2
         constantEnemyHp = enemyHp
-        updateHpElement();
+        updateElements();
         handleExp(100);
     } else {
-        return
+        return;
     }
-}
+};
 
 normalAttackElement.addEventListener('click', () => {
+    totalClicks++;
+    ultClicks++;
+    handleUltClicks();
     normalAttack(calculateDamage('normal'));
 });
 
 ultAttackElement.addEventListener('click', () => {
+    totalClicks++;
+    ultClicks++;
+    handleUltClicks();
     ultAttack(calculateDamage('ult'));
 });
 
 miscElement.addEventListener('click', () => {
+    totalClicks++;
     specialAbil();
 });
